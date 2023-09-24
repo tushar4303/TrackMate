5f40a11379c258178813e2681958a75911eecf53
PurpleVen <vendra0408@gmail.com>
2023-09-24 22:44:29 +0530

    db changes

--- install.php
+++ install.php
@@ -30,8 +30,8 @@
 			`email` varchar(50) NOT NULL,
 			`fingerprint_id` int(11) NOT NULL,
 			`fingerprint_select` tinyint(1) NOT NULL DEFAULT '0',
-			`user_date` date NOT NULL,
-			`time_in` time NOT NULL,
+			`user_date` date,
+			`enrolled_at` time,
 			`del_fingerid` tinyint(1) NOT NULL DEFAULT '0',
 			`add_fingerid` tinyint(1) NOT NULL DEFAULT '0'
 	) ENGINE=InnoDB DEFAULT CHARSET=latin1";
@@ -47,9 +47,8 @@
 			`username` varchar(100) NOT NULL,
 			`serialnumber` double NOT NULL,
 			`fingerprint_id` int(5) NOT NULL,
-			`checkindate` date NOT NULL,
-			`timein` time NOT NULL,
-			`timeout` time NOT NULL
+			`checkin_date` date NOT NULL,
+			`checkin_time` time
 	) ENGINE=InnoDB DEFAULT CHARSET=latin1";
 
 	if ($conn->query($sql) === TRUE) {
